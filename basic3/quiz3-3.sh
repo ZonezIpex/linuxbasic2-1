d2b53104de176b6b001e937007749049d76d1509
ZonezIpex <162661168+ZonezIpex@users.noreply.github.com>
2024-04-17 12:25:30 +0900

    4월 17일 커밋

--- basic3/quiz3-3.sh
+++ basic3/quiz3-3.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -s "quiz3-2.sh" ] && [ -e "quiz3-2.sh" ]
 then
-    echo "yes file"
+    echo "파일이 존재하며 비어있는 파일이 아닙니다"
 else
-    echo "none file"
+    echo "존재하지 않거나, 비어있는 파일입니다"
 fi
 exit 0
